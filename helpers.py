90824ce5e521cd2179e251412483a3d42d6056c5
Nik Clement <nikc@cloudone.co.nz>
2019-03-17 09:50:32 +1100

    Update help text Made a few changes to increase the ability of players to start solving puzzles

--- helpers.py
+++ helpers.py
@@ -5,7 +5,7 @@
 def help_contents():
     """Lists all of the commands and some text about what they do"""
     print('Commands I know are:')
-    print('north, south, west, east - move in that direction')
+    print('north, south, west, east, up, down - move in that direction, short command of first letter also available')
     print('talk - hear what the room inhabitant has to say')
     print('hug - hug the room inhabitant, be careful not to hug enemies')
     print('take, get - pick up an item')
@@ -14,7 +14,7 @@ def help_contents():
     print('inv - display the contents of my backpack')
     print('fight - challenge the room inhabitant to a duel with an item')
     print('hum - a way of waiting for something to happen')
-    print('climb - some places have things that you can climb')
+    print('climb - some places have things that you can climb, including trees')
     print('swim - swim in a body of water')
     print('pick - pick flowers or herbs from this location')
     print('score - print the scores so far')
